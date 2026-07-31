1bef4661957c0d2b28f99d3883010605647bb611
Vikramthakur575 <vt21928@gmail.com>
2026-07-31 21:02:39 +0530

    Time: 183 ms (33.11%) | Memory: 19.8 MB (93.65%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        # Frequency of each letter
+        freq = [0] * 26
+
+        for ch in word:
+            freq[ord(ch) - ord('a')] += 1
+
+        # Highest frequency first
+        freq.sort(reverse=True)
+
+        pushes = 0
+
+        # Assign cheapest positions first
+        for i, f in enumerate(freq):
+
+            if f == 0:
+                break
+
+            cost = i // 8 + 1
+
+            pushes += f * cost
+
+        return pushes
+        
