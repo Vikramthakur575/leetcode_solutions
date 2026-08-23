717f892d8732edacb4d6415a99fc96ea41b04754
Vikramthakur575 <vt21928@gmail.com>
2026-08-23 14:22:06 +0530

    Time: 60 ms (35.29%) | Memory: 19.9 MB (65.29%) - LeetSync

---
+++ 2039-sum-game/sum-game.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+
+        n = len(num)
+        half = n // 2
+
+        left_sum = 0
+        right_sum = 0
+        left_q = 0
+        right_q = 0
+
+        for i in range(half):
+            if num[i] == '?':
+                left_q += 1
+            else:
+                left_sum += int(num[i])
+
+        for i in range(half, n):
+            if num[i] == '?':
+                right_q += 1
+            else:
+                right_sum += int(num[i])
+
+        diff = left_sum - right_sum
+
+        # Odd number of '?' -> Alice can force a win
+        if (left_q + right_q) % 2:
+            return True
+
+        # Alice can force a win unless Bob can balance the difference
+        return abs(diff * 2 + (left_q - right_q) * 9) != 0
+        
